ee8f804f971f6c9b640ac386f38f435be8b413c4
esdb1 <pierre.lacognata@mail-esd.com>
2023-11-29 10:50:19 +0100

    stuff

---
+++ src/app/autrepage/autrepage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-autrepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './autrepage.component.html',
+  styleUrl: './autrepage.component.css'
+})
+export class AutrepageComponent {
+
+}
